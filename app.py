96c3e0f28086d813a6982cd0b9e0c882d87c0448
Emerson Bilieri <ebilieri@hotmail.com>
2020-08-03 16:55:32 -0300

    Adicionado autenticacao na aplicação

--- app.py
+++ app.py
@@ -1,12 +1,32 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
-from models import Pessoas
+from models import Pessoas, Usuarios
+from flask_httpauth import HTTPBasicAuth
 
+auth = HTTPBasicAuth()
 app = Flask(__name__)
 api = Api(app)
 
+# USUARIOS = {
+#     'emerson': '123',
+#     'bilieri': '321'
+# }
+#
+#
+# @auth.verify_password
+# def verificacao(login, senha):
+#     if not (login, senha):
+#         return False
+#     return USUARIOS.get(login) == senha
+
+@auth.verify_password
+def verificacao(login, senha):
+    if not (login, senha):
+        return False
+    return Usuarios.query.filter_by(login=login, senha=senha).first()
 
 class Pessoa(Resource):
+    @auth.login_required
     def get(self, nome):
         pessoa = Pessoas.query.filter_by(nome=nome).first()
         try:
@@ -22,6 +42,7 @@ class Pessoa(Resource):
             }
         return response
 
+    @auth.login_required
     def put(self, nome):
         pessoa = Pessoas.query.filter_by(nome=nome).first()
         try:
@@ -44,6 +65,7 @@ class Pessoa(Resource):
             }
         return response
 
+    @auth.login_required
     def delete(self, nome):
         pessoa = Pessoas.query.filter_by(nome=nome).first()
         try:
@@ -65,9 +87,10 @@ class ListaPessoas(Resource):
     def get(self):
         pessoas = Pessoas.query.all()
         print(pessoas)
-        response = [{'id': i.id, 'nome':i.nome, 'idade':i.idade} for i in pessoas]
+        response = [{'id': i.id, 'nome': i.nome, 'idade': i.idade} for i in pessoas]
         return response
 
+    @auth.login_required
     def post(self):
         dados = request.json
         pessoa = Pessoas(nome=dados['nome'], idade=dados['idade'])
--- utils.py
+++ utils.py
@@ -1,30 +1,49 @@
-from models import Pessoas, db_session
+from models import Pessoas, Usuarios
+
 
 def insere_pessoas():
     pessoa = Pessoas(nome='Joao Lucas', idade=5)
     pessoa.save()
     print(pessoa)
 
+
 def altera_pessoa():
     pessoa = Pessoas.query.filter_by(nome='Emerson').first()
     pessoa.idade = 25
     pessoa.save()
 
+
 def exclui_pessoa():
     pessoa = Pessoas.query.filter_by(nome='Joao Lucas').first()
     pessoa.delete()
 
+
 def consulta():
     pessoa = Pessoas.query.filter_by(nome='Joao Lucas').first()
-    print('{} idade: {}' .format(pessoa, pessoa.idade))
+    print('{} idade: {}'.format(pessoa, pessoa.idade))
+
 
 def consulta_all():
     pessoa = Pessoas.query.all()
     print(pessoa)
 
+
+def insere_usuario(login, senha):
+    usuario = Usuarios(login=login, senha=senha)
+    usuario.save()
+
+
+def consulta_todos_usuarios():
+    usuarios = Usuarios.query.all()
+    print(usuarios)
+
+
 if __name__ == '__main__':
-    #insere_pessoas()
-    #altera_pessoa()
-    #exclui_pessoa()
-    consulta_all()
-    consulta()
+    # insere_pessoas()
+    # altera_pessoa()
+    # exclui_pessoa()
+    # consulta_all()
+    # consulta()
+    #insere_usuario('emerson', '1234')
+    #insere_usuario('bilieri', '4321')
+    consulta_todos_usuarios()
